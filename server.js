3041f4c18bef9766644bd4c328f13a54974acdac
Leonardo1596 <leonardoemanuel156@gmail.com>
2025-09-13 03:40:46 -0300

    changed cors options

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const { generateReportJob } = require('./controllers/HistoryReportController');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'https://gestaobarber.netlify.app', // apenas seu frontend
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // cabeçalhos que você usa
+    credentials: true // se você usar cookies/autenticação
+}));
+app.options('*', cors());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
